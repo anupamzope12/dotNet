ec8631fe267743c1d3f6b9048dac34540dea4975
Anupam Zope <anupamzope02@gmail.com>
2023-10-07 09:53:07 +0530

    Movie Mania Project by MVC Framework

---
+++ MovieMania/MovieMania/Models/Movie.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Org.BouncyCastle.Asn1.Mozilla;
+using System.ComponentModel.DataAnnotations;
+
+namespace MovieMania.Models
+{
+    [Index(nameof(MovieName),IsUnique =true)]
+    public class Movie
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required(ErrorMessage ="Movie Name is required")]
+        public string MovieName { get; set; }
+
+        [Required(ErrorMessage = "Producer Name is required")]
+        public string producer { get;set; }
+
+        public double ticketPrice { get; set; }
+
+        public DateOnly releaseDate { get; set; }
+
+
+    }
+}
